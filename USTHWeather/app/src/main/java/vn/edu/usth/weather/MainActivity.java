b554cefee3a4e138d68596b3f934163fdfb212bd
DDD1601 <144022311+DDD1601@users.noreply.github.com>
2024-09-12 12:54:21 +0700

    Update MainActivity.java

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/MainActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -1,24 +1,70 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private WeatherPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_weather);
+
+
+        viewPager = findViewById(R.id.viewPager);
+
+
+        pagerAdapter = new WeatherPagerAdapter(getSupportFragmentManager());
+        setupViewPager(viewPager);
+
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        WeatherPagerAdapter adapter = new WeatherPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new WeatherAndForecastFragment(), "Weather and Forecast 1");
+        adapter.addFragment(new WeatherAndForecastFragment(), "Weather and Forecast 2");
+        adapter.addFragment(new WeatherAndForecastFragment(), "Weather and Forecast 3");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    private static class WeatherPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public WeatherPagerAdapter(FragmentManager manager) {
+            super(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }
