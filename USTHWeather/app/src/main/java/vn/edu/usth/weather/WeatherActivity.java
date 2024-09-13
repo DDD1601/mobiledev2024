cb0907bcc0eb4aac97caafe140ef9c542b2cc1c1
DDD1601 <144022311+DDD1601@users.noreply.github.com>
2024-09-13 13:18:26 +0700

    practical8

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,68 +1,67 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager.widget.ViewPager;
 import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
-
-    private static final String TAG = "WeatherActivity";
-    private ViewPager viewPager;
-    private WeatherPagerAdapter weatherPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        // Apply window insets for immersive experience
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
 
-        Log.i(TAG, "onCreate called");
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
 
-        // Set up ViewPager and PagerAdapter
-        viewPager = findViewById(R.id.viewPager);
-        weatherPagerAdapter = new WeatherPagerAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(weatherPagerAdapter);
+
+//        ForecastFragment firstFragment = new ForecastFragment();
+//        getFragmentManager().beginTransaction().add(
+//            R.id.main, firstFragment).commit();
+//        Log.i("Weather", "onCreate()");
     }
 
-    // Lifecycle logging methods
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart called");
+        Log.i("Weather", "onStart()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "onResume called");
+        Log.i("Weather", "onResume()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "onPause called");
+        Log.i("Weather", "onPause()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "onStop called");
+        Log.i("Weather", "onStop()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroy called");
+        Log.i("Weather", "onDestroy()");
     }
 }
-
